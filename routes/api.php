4977b4901c03c2a9468c696a3e5f82932903cc82
IuryHirabara <dev.iuryhirabara@gmail.com>
2025-08-11 14:09:55 -0300

    chore: add route to show balance from an account

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,11 @@
 <?php
 
+use App\Http\Controllers\V1\{BalanceController};
 use Illuminate\Support\Facades\Route;
+
+Route::name('v1.')->group(function () {
+    Route::controller(BalanceController::class)->prefix('balance')->name('balance.')
+        ->group(function () {
+            Route::get('', 'show')->name('show');
+        });
+});
