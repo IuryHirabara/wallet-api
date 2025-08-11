34bc41d95ff3a6e03c6d0d847a2893f852e779ef
IuryHirabara <dev.iuryhirabara@gmail.com>
2025-08-11 16:42:36 -0300

    chore: add. route for events

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
-use App\Http\Controllers\V1\{BalanceController};
+use App\Http\Controllers\V1\{BalanceController, EventController};
 use Illuminate\Support\Facades\Route;
 
 Route::name('v1.')->group(function () {
     Route::controller(BalanceController::class)->prefix('balance')->name('balance.')
         ->group(function () {
             Route::get('', 'show')->name('show');
+        });
+
+    Route::controller(EventController::class)->prefix('event')->name('event.')
+        ->group(function () {
+            Route::post('', 'store')->name('store');
         });
 });
