6d622e42a1224b28d9e33be70808dcf89a8b3898
IuryHirabara <dev.iuryhirabara@gmail.com>
2025-08-12 16:02:34 -0300

    chore: create a class to set money operation strategy

---
+++ app/Services/V1/Wallet/WalletManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\V1\Wallet;
+
+use App\Contracts\V1\PerformsMoneyOperation;
+
+class WalletManager
+{
+    private PerformsMoneyOperation $moneyOperation;
+
+    public function doMoneyOperation(int $amount)
+    {
+        return $this->moneyOperation->doMoneyOperation($amount);
+    }
+
+    public function setMoneyOperation(PerformsMoneyOperation $moneyOperation): self
+    {
+        $this->moneyOperation = $moneyOperation;
+
+        return $this;
+    }
+}
