cb0715d9d61ab55d2cefe711e1e10e8a259ef9cd
IuryHirabara <dev.iuryhirabara@gmail.com>
2025-08-12 15:47:28 -0300

    chore: implement the new method from RecoversAccount contract

--- app/Services/V1/AccountRetriever.php
+++ app/Services/V1/AccountRetriever.php
@@ -4,11 +4,23 @@ namespace App\Services\V1;
 
 use App\Contracts\V1\RecoversAccount;
 use App\Models\Account;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class AccountRetriever implements RecoversAccount
 {
     public function findById(string $id): ?Account
     {
         return Account::where('id', $id)->first();
+    }
+
+    public function findByIdOrFail(string $id): Account
+    {
+        $account = $this->findById($id);
+
+        if (!$account) {
+            throw new NotFoundResourceException("Account with id '$id' not found.");
+        }
+
+        return $account;
     }
 }
